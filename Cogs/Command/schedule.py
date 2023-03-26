87bf9509cb6d0077ff96465a4204c7887a565119
yng._.min <0min1320@kakao.com>
2023-03-26 11:11:15 +0900

    .

--- Cogs/Command/schedule.py
+++ Cogs/Command/schedule.py
@@ -53,16 +53,6 @@ class ScheduleButton(discord.ui.View):
         self.msg = msg
         self.banner = banner
 
-        # print(box_1_match_schedule_2)
-        # print(msg_schedule_info_1)
-        # print(msg_schedule_info_1_2)
-        # print(box_2_match_schedule_2)
-        # print(msg_schedule_info_2)
-        # print(msg_schedule_info_2_2)
-        # print(box_3_match_schedule_2)
-        # print(msg_schedule_info_3)
-        # print(msg_schedule_info_3_2)
-
         if box_1_match_schedule_2 == "해당 일자의 리그 일정이 없습니다.":
             self.schedule_1_1 = []
             self.schedule_1_2 = []
