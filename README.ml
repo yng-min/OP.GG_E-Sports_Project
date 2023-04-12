bb3772a69559900cad9e0fd1e13925796bb369ff
yng._.min <0min1320@kakao.com>
2023-04-12 23:17:08 +0900

    .

---
+++ README.ml
@@ -0,0 +1,48 @@
+# Hello, World! (English)
+
+Welcome to '[OP.GG E-Sports](https://esports.op.gg/) for Discord' Repository.
+
+## Notice.
+
+First of all, This repository and source-code is **NOT** Open Source.
+The copyright and ownership of this source code is in [OP.GG](https://op.gg/) and @yng-min.
+
+So, it's not marked with a license.
+
+**DO NOT** use this source-code personally/commercially except for OP.GG officials.
+
+## What is this Project?
+
+This project is to make the OP.GG E-Sports service operated by OP.GG available on Discord. (Discord Bot)
+
+## Why this Project is not private repository?
+
+The reason is to exchange quick and smooth feedback with OP.GG staff.
+
+If it's a private repository, because there are unnecessary and cumbersome processes in the feedback with many people.
+So I decided to share it as a public repository.
+
+
+
+# Hello, Wolrd! (Korean)
+
+[오피지지 e스포츠](https://esports.op.gg/) 디스코드 봇 리포지토리입니다.
+
+## 공지사항
+
+가장 먼저, 이 리포지토리와 소스 코드는 **오픈 소스가 아닙니다.**
+이 프로젝트의 저작권과 소유권은 [OP.GG](https://op.gg/)와 @yng-min에게 있습니다.
+
+따라서 별도로 표기된 라이선스가 존재하지 않습니다.
+
+그렇기 때문에 오피지지 직원 외에는 이 소스 코드를 개인적 또는 상업적 사용을 **절대** 금합니다.
+
+## 이 프로젝트는 무엇인가요?
+
+이 프로젝트는 오피지지에서 운영 중인 오피지지 e스포츠 서비스의 디스코드 봇 버전입니다.
+
+## 오픈 소스가 아니라면 왜 공개 리포지토리인가요?
+
+오피지지 관계자와의 빠르고 원활한 피드백을 주고받기 위함입니다.
+
+비공개 리포지토리일 경우, 많은 사람들과의 피드백 과정에서 불필요한 과정이나 귀찮은 과정이 많아지므로 공개 리포지토리로 공유하기로 결정했습니다.
