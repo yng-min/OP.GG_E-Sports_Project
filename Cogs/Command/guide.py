b59c25fa4929c65cfc9ba4d17265b30dea1007a5
yng._.min <0min1320@kakao.com>
2023-05-01 01:22:50 +0900

    EDIT: '/가이드' 명령어에 '/밴픽 순위' 명령어에 대한 설명 추가

--- Cogs/Command/guide.py
+++ Cogs/Command/guide.py
@@ -32,6 +32,7 @@ def embed_setup(bot, banner, page):
         embed = discord.Embed(title="> 📌 OP.GG e스포츠 봇 서비스 가이드", description="```리그 명령어 페이지```", color=colorMap['red'])
         embed.add_field(name="/경기 일정", value="리그 오브 레전드 e스포츠의 경기 일정을 확인할 수 있어요.", inline=False)
         embed.add_field(name="/리그 순위", value="리그 오브 레전드 e스포츠의 시즌 팀 순위를 보여줘요.", inline=False)
+        embed.add_field(name="/밴픽 순위", value="리그 오브 레전드 e스포츠의 밴/픽 순위를 불러와요.", inline=False)
         embed.add_field(name="/베스트 플레이어", value="리그 오브 레전드 e스포츠의 시즌 MVP는 누구인지 찾아보세요!", inline=False)
         embed.add_field(name="/선수 검색", value="리그 오브 레전드 e스포츠의 선수 정보를 검색해요.", inline=False)
 
